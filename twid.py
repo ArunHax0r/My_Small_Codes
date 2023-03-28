015278d6ac5423758cdb4bc856756c01b5858585
ArunHax0r <129146655+ArunHax0r@users.noreply.github.com>
2023-03-28 12:31:31 +0530

    Update and rename fid.py to twid.py

--- twid.py
+++ twid.py
@@ -2,11 +2,11 @@ import webbrowser as wb
 
 # Enter your User Name
 uid = input("Enter the user name: ")
-url = "www.facebook.com/"
+url = "twitter.com/"
 
 # It opens in your Default browser and Checks for the Username in Facebook website
-def getFbid():
+def getTwid():
     print('Fetching details...')
     wb.open(url + uid, new=0, autoraise=True)
     
-getFbid()
+getTwid()
