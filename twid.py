5b2cd845472820abb316277e1866c1024f36f038
ArunHax0r <129146655+ArunHax0r@users.noreply.github.com>
2023-03-28 12:35:00 +0530

    Update twid.py

--- twid.py
+++ twid.py
@@ -4,7 +4,7 @@ import webbrowser as wb
 uid = input("Enter the user name: ")
 url = "twitter.com/"
 
-# It opens in your Default browser and Checks for the Username in Facebook website
+# It opens in your Default browser and Checks for the Username in Twitter website
 def getTwid():
     print('Fetching details...')
     wb.open(url + uid, new=0, autoraise=True)
