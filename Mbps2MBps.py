bc7a930723431ea98f5ebd3d1544691b6855e4c6
ArunHax0r <129146655+ArunHax0r@users.noreply.github.com>
2023-10-21 11:38:40 +0530

    Update and rename mb2MB.py to Mbps2MBps.py

---
+++ Mbps2MBps.py
@@ -0,0 +1,4 @@
+megabits = int(input('Enter your speed(Mbps): '))
+megabytes = megabits*0.125
+print(f'Your original speed is: {megabytes}MB/s')
+    
--- mb2MB.py
+++
@@ -1,4 +0,0 @@
-mbits = int(input('Enter your speed(Mbps): '))
-mbytes = mbits*0.125
-print(f'Your original speed is: {mbytes}MB/s')
-    
